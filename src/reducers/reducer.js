5642345818bd21c92589e55c836b953b89f35556
Manon <manon.jeuland@gmail.com>
2021-03-23 16:07:52 +0100

    installing dependencies redux, router,...

---
+++ src/reducers/reducer.js
@@ -0,0 +1,6 @@
+import {combineReducers} from "redux";
+import front from './front'
+//si autre reducer, il faut l'importer au dessus et le rajouter dans les accolades en dessous
+
+
+export default combineReducers({front});
